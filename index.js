6dd2bafe440d690e5216c7c107dfec41356ded5d
AndresGomez99 <48743247+AndresGomez99@users.noreply.github.com>
2021-03-01 12:52:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,8 @@ const app= express();
 
 app.set('view engine','ejs');
 app.get("/",(req,res)=>{
-    
+      res.render("login")    
 });
 
-
-app.get("/login",(req,res)=>{
-    res.render("login")
-});
-
-
-app.post("/login",(req,res)=>{
-    
-});
 app.listen(process.env.PORT || 5000)
 
